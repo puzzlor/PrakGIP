949768dbe3b40671577354ba37314881b7971fb2
puzzlor <puzzlor@users.noreply.github.com>
2014-12-10 11:27:50 +0100

    Create 01B.cpp

---
+++ 01B.cpp
@@ -0,0 +1,28 @@
+// Datei: main.cpp
+//-------------------------------------------B01.01
+#include <iostream> 
+using namespace std;
+
+int main(){
+
+	int Eingabe = 0;
+	cout << "Bitte geben Sie Ihre Studienrichtung ein: " << endl;
+	cout << "1: Informatiker" << endl;
+	cout <<"2: Elektrotechniker" << endl;
+	cin >> Eingabe;
+	cout << "Ihre Eingabe lautete: " << Eingabe << endl;
+	system("PAUSE");
+	return 0;
+}
+//-------------------------------------------B01.02
+//#include <iostream> 
+//#include <limits.h> 
+//using namespace std;
+//
+//int main(){
+//	cout << "Die groesste speicherbare Integerzahl lautet: " << INT_MAX << endl;
+//	cout << "Die kleinste speicherbare Integerzahl lautet: " << INT_MIN << endl;
+//
+//	system("PAUSE");
+//	return 0;
+//}
