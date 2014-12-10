4c7211e865528086ca0d66dfdaf6207901470617
puzzlor <puzzlor@users.noreply.github.com>
2014-12-10 11:33:35 +0100

    Create 04AFunktionen.h

---
+++ 04AFunktionen.h
@@ -0,0 +1,8 @@
+// Datei: Funktionen.h
+#include <string> 
+#pragma once 
+using namespace std;
+// Aufgabe A04.01 
+int zeichenkette_finden_iterativ(string text, string zkette);
+// Aufgabe A04.02 
+int zeichenkette_finden_rekursiv(string text, string zkette, unsigned int text_pos = 0, unsigned int text_such_pos = 0, unsigned int zkette_such_pos = 0);
